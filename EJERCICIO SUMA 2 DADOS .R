d7ff2df155ae13fb9d842a0ac568c3875ad3a1da
Yamiletsy <alejandrabetsy@unam.edu>
2026-03-12 14:54:28 -0700

    Add files via upload

---
+++ EJERCICIO SUMA 2 DADOS .R
@@ -0,0 +1,26 @@
+pob = 1:6
+res = c()
+start.time <- Sys.time()
+
+for (j in 1:100){
+  dados = sample(pob, 2, replace = TRUE)   
+  suma = sum(dados)                        
+  
+  res <- c(res, suma)
+  
+  plot(0,0,xlim=c(-50,50), ylim=c(-50,50), axes = FALSE, type='n', xlab = "", ylab = "")
+  # dado 1
+  text(0,10, dados[1], cex=10)  
+  # dado 2
+  text(0,-10, dados[2], cex=10) 
+  # suma
+  text(0,30, paste("Suma =", suma), cex=2) 
+  
+  Sys.sleep(1)
+}
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+print(time.taken)
+
+hist(res, breaks=2:13, right = F)
